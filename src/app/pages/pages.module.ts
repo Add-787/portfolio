2aae3240e2374e18a7ff10876a20976332423dc4
adarshshetty <adarshshetty787@gmail.com>
2023-02-26 17:20:32 +0530

    Commits made: - Inline style language changed to scss. - Added statistics component. - Added intro boxes.

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { IntroComponent } from './home/intro/intro.component';
+import { StatsComponent } from './home/stats/stats.component';
 
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    IntroComponent
+    IntroComponent,
+    StatsComponent
   ],
   imports: [
     CommonModule
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HighlightDirective } from './highlight.directive';
 
 
 
 @NgModule({
   declarations: [
-    NavbarComponent
+    NavbarComponent,
+    HighlightDirective
   ],
   imports: [
     CommonModule
